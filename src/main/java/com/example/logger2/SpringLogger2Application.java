8e825815e2cb0f5f8830453f72be168984770f93
seanjungmsba <seanjeong1996@gmail.com>
2022-08-10 12:38:56 -0400

    Modified logback.xlm to configure output file

---
+++ src/main/java/com/example/logger2/SpringLogger2Application.java
@@ -0,0 +1,11 @@
+package com.example.logger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogger2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogger2Application.class, args);
+	}
+}
